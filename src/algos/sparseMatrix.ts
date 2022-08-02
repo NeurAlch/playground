997f67f37c9e20e7a6a6ccecc63a2e50bb83d2ac
Pablo Rosales <me@pablorosales.xyz>
2022-08-01 20:09:05 -0600

    Add sparse matrix CSR (compressed sparse row)

---
+++ src/algos/sparseMatrix.ts
@@ -0,0 +1,39 @@
+export interface CSR {
+  col: number[];
+  row: number[];
+  value: number[];
+  shape: [number, number];
+}
+
+export const sparseMatrixToCSR = (matrix: number[][]): CSR => {
+  const csr: CSR = {
+    row: [],
+    col: [],
+    value: [],
+    shape: [0, 0],
+  };
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== 0) {
+        csr.row.push(i);
+        csr.col.push(j);
+        csr.value.push(matrix[i][j]);
+      }
+    }
+  }
+
+  csr.shape = [matrix.length, matrix[0].length];
+
+  return csr;
+};
+
+export const sparseMatrixFromCSR = (obj: CSR): number[][] => {
+  const matrix: number[][] = Array(obj.shape[0])
+    .fill(0)
+    .map(() => Array(obj.shape[1]).fill(0));
+  for (let i = 0; i < obj.row.length; i++) {
+    matrix[obj.row[i]][obj.col[i]] = obj.value[i];
+  }
+  return matrix;
+};
---
+++ src/tests/unit-tests/algos/sparseMatrix.test.ts
@@ -0,0 +1,42 @@
+import { CSR, sparseMatrixFromCSR, sparseMatrixToCSR } from '../../../algos/sparseMatrix';
+
+interface Sample {
+  to: CSR;
+  from: number[][];
+}
+
+describe('sparseMatrix', () => {
+  it('should return a sparse matrix represented as row,col,val', () => {
+    const sample1: Sample = {
+      from: [
+        [0, 0, 1],
+        [0, 1, 0],
+        [1, 0, 0],
+      ],
+      to: {
+        shape: [3, 3],
+        col: [2, 1, 0],
+        row: [0, 1, 2],
+        value: [1, 1, 1],
+      },
+    };
+    expect(sparseMatrixToCSR(sample1.from)).toEqual(sample1.to);
+    expect(sparseMatrixFromCSR(sample1.to)).toEqual(sample1.from);
+
+    const sample2: Sample = {
+      from: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 3, 1, 0, 0, 0, 0, 0],
+        [1, 2, 0, 1, 0, 0, 0, 0, 0],
+      ],
+      to: {
+        shape: [3, 9],
+        col: [2, 3, 0, 1, 3],
+        row: [1, 1, 2, 2, 2],
+        value: [3, 1, 1, 2, 1],
+      },
+    };
+    expect(sparseMatrixToCSR(sample2.from)).toEqual(sample2.to);
+    expect(sparseMatrixFromCSR(sample2.to)).toEqual(sample2.from);
+  });
+});
