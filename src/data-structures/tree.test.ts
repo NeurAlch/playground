f930084ecb6451254db743db4d402f1e2b691525
Pablo Rosales <me@pablorosales.xyz>
2022-11-07 22:59:50 -0600

    Update

--- src/data-structures/tree.test.ts
+++ src/data-structures/tree.test.ts
@@ -2,6 +2,8 @@ import { Tree } from './tree';
 
 describe('tree', () => {
   it('should create a tree', () => {
-    const tree = new Tree<number>();
+    const tree = new Tree<number>(1);
+    expect(tree).toBeInstanceOf(Tree);
+    expect(tree.toArray().length).toBe(0);
   });
 });
--- src/data-structures/tree.ts
+++ src/data-structures/tree.ts
@@ -1,9 +1,43 @@
-interface ITreeNode {
+interface ITreeNode<TValue> {
+  value: TValue;
+  toArray(): TValue[];
 }
 
 interface ITree<TValue> {
+  toArray(): TValue[];
+  remove(value: TValue): void;
+}
+
+export class TreeNode<TValue> implements ITreeNode<TValue> {
+  private _value: TValue;
+  private _children: ITreeNode<TValue>[] = [];
+
+  constructor(defaultValue: TValue) {
+    this._value = defaultValue;
+    this._children = [];
+  }
+
+  public get value(): TValue {
+    return this._value;
+  }
+
+  public toArray(): TValue[] {
+    return [];
+  }
 }
 
 export class Tree<TValue> implements ITree<TValue> {
+  private root: TreeNode<TValue>;
 
+  constructor(rootValue: TValue) {
+    this.root = new TreeNode<TValue>(rootValue);
+  }
+
+  public remove(value: TValue) {
+    value;
+  }
+
+  public toArray(): TValue[] {
+    return this.root.toArray();
+  }
 }
